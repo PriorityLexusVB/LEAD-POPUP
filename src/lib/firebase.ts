5044266ed7107c601c48d48d57cb5717280a27e2
Rob Brasco <rob.brasco@priorityautomotive.com>
2025-09-06 17:11:27 +0000

    still not working!!!!!!!!!!!!!!!!!!!!!!!!

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,27 +3,17 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "priority-lead-sync",
-  "appId": "1:27409891046:web:6816fad326e6f7b0c28527",
-  "storageBucket": "priority-lead-sync.appspot.com",
-  "apiKey": "AIzaSyACeCULtIczV2Jb7rdbYctDY82FaB2GTsA",
-  "authDomain": "priority-lead-sync.firebaseapp.com",
-  "messagingSenderId": "27409891046"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // Point to the default database to match the function.
 const db = getFirestore(app);
-
-// If you want to connect to the Firestore emulator, uncomment the following lines
-// if (process.env.NODE_ENV === 'development') {
-//   try {
-//     connectFirestoreEmulator(db, 'localhost', 8080);
-//     console.log("Connected to Firestore emulator");
-//   } catch (error) {
-//     console.error("Error connecting to Firestore emulator", error);
-//   }
-// }
 
 export { db };
