ef2539ef2af50317e8272335c92d43c61a2ff18b
Rob Brasco <rob.brasco@priorityautomotive.com>
2025-09-06 16:32:36 +0000

    NOPE - SOMETHING BROKE OVER THE LAST 30 MINUTES, REVIEW OUR CHANGES

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "priority-lead-sync-jts63",
+  "projectId": "priority-lead-sync",
   "appId": "1:27409891046:web:6816fad326e6f7b0c28527",
-  "storageBucket": "priority-lead-sync-jts63.appspot.com",
+  "storageBucket": "priority-lead-sync.appspot.com",
   "apiKey": "AIzaSyACeCULtIczV2Jb7rdbYctDY82FaB2GTsA",
-  "authDomain": "priority-lead-sync-jts63.firebaseapp.com",
+  "authDomain": "priority-lead-sync.firebaseapp.com",
   "messagingSenderId": "27409891046"
 };
 
