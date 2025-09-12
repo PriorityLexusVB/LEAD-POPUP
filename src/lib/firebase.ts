2f4b73e5589bf0c8ec48ae0afb293325160d27dc
Rob Brasco <rob.brasco@priorityautomotive.com>
2025-09-12 23:33:08 +0000

    Priority Lead Sync Failed to connect to Firestore. Please check your con

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 const firebaseConfig = {
   "projectId": "priority-lead-sync-jts63",
   "appId": "1:27409891046:web:6816fad326e6f7b0c28527",
-  "storageBucket": "priority-lead-sync-jts63.firebasestorage.app",
+  "storageBucket": "priority-lead-sync-jts63.appspot.com",
   "apiKey": "AIzaSyACeCULtIczV2Jb7rdbYctDY82FaB2GTsA",
   "authDomain": "priority-lead-sync-jts63.firebaseapp.com",
   "messagingSenderId": "27409891046"
